6fe3f7c03e217ef9527ede1ed0fa4f3b1d4793c9
intingenza <50455606+intingenza@users.noreply.github.com>
2019-05-09 17:18:22 +0200

    added main.py and requirements.txt

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import sendgrid
+from sendgrid.helpers.mail import *
+
+# Build mail
+sg = 
+sendgrid.SendGridAPIClient(api_key=os.environ.get("SENDGRID_API_KEY"))
+
+from_email = Email(os.environ.get("SENDER"))
+
+to_email = Email(os.environ.get("RECEIVER"))
+
+email = Mail()
+
+email.from_email = from_email
+email.subject = 'Made it to send email'
+
+personalization = Personalization()
+personalization.add_to(to_email)
+
+email.add_personalization(personalization)
+email.add_content('')
+
+# Send mail
+try:
+    response = sg.send(email.get())
+except Exception as e:
+    raise e
+
